bfb377b0aa0259a7acf2c22645f2ebf9d6c3fd14
Mikolaj J <mikkiodev@gmail.com>
2024-06-02 17:05:19 +0200

    feat: add a_variables exercise solution

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,14 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let (mut missiles, ready) = (STARTING_MISSILES, READY_AMOUNT);
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    let _test = 1;
+
+    missiles = missiles - ready;
+
+    println!("{} missiles left", missiles);
+}
